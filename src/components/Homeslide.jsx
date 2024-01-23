f4f7c793283965dad61ed0cdca1c47d18c7fafac
Sandra Yeboah <119595994+sandrayeboahAE@users.noreply.github.com>
2024-01-23 16:50:41 +0000

    added homeslide

--- src/components/Homeslide.jsx
+++ src/components/Homeslide.jsx
@@ -1,19 +1,18 @@
-import { Flex, Text, Title } from "@mantine/core";
+import { Flex, Text } from "@mantine/core";
 import Link from "next/link";
 import React from "react";
 
 const HomeSlide = () => {
     return ( <div>
-        <div className="bg-black p-5">
+        <div className="homeslide flex items-center justify-center">
             <Flex direction="column" align="center" justify="center">
                 <div className="m-5">
-                  <Text c="white" fz="md">WELCOME TO FOHOLIC</Text>
+                  <Text c="white" fz="xl">WELCOME TO FOHOLIC</Text>
                 </div>
                 <div className="p-5">
-                    <Flex direction="column" align="center" gap="md">
-                       <Title order={1} className="text-white">We do not just cook we</Title> 
-                       <Title order={1} className="text-white">thread you to</Title>
-                       <Title order={1} className="text-white">delicacies... </Title>
+                    <Flex direction="column" align="center">
+                       <p className="headers">We do not just cook we</p> 
+                       <p className="headers">thread you to delicacies... </p>
                     </Flex>
                 </div>
                 <div className="border border-white text-white p-4 m-5">
